468b4cf26a2d3553aee076188f2de7d785330a94
SindhuGangi <gangisindhu2000@gmail.com>
2025-06-29 00:56:06 +0530

    done with header

---
+++ javascrpt.js
@@ -0,0 +1,33 @@
+var createcontent = document.querySelector(".container.playgame").style.visibility = "hidden";
+var warning = document.querySelector("p.caption1.caption2");
+warning.style.visibility = "hidden";
+iswarning = false;
+function checkinput() {
+  var text = document.querySelector("input").value;
+  var text1 = document.querySelectorAll("input")[1].value;
+  if (((text.trim() === "") || (text1.trim() === ""))) {
+    warning.textContent = "❌ Both fields are required.";
+    warning.style.visibility = "visible";
+  }
+
+  if (!Number.isInteger(Number(text)) || !Number.isInteger(Number(text1))) {
+    warning.textContent = "❌ Both inputs must be whole numbers.";
+    warning.style.visibility = "visible";
+  }
+  else if ((Number(text) > 5 || Number(text) < 2) && (Number(text1) > 6 || Number(text1) < 1)) {
+    warning.textContent = "❌ Both inputs must be  Players-2:5 and  Dices-1:6 .";
+    warning.style.visibility = "visible";
+  }
+  else if (Number(text) > 5 || Number(text) < 2) {
+    warning.textContent = "❌ Players-2:5.";
+    warning.style.visibility = "visible";
+
+  } else if (Number(text1) > 6 || Number(text1) < 1) {
+    warning.textContent = "❌ Dices-1:6.";
+    warning.style.visibility = "visible";
+
+  }
+
+}
+
+
