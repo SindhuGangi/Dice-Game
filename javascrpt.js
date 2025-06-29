eff678688b2708bd1b313351ca50e4d293b9d9b4
SindhuGangi <gangisindhu2000@gmail.com>
2025-06-30 04:03:50 +0530

    Updated Js

--- javascrpt.js
+++ javascrpt.js
@@ -1,7 +1,14 @@
 var createcontent = document.querySelector(".container.playgame");
+var startGame = document.querySelector(".container");
+var main1 = document.querySelector(".container3");
+var main2 = document.querySelector(".container3.container4");
+main2.style.display = "none";
 createcontent.style.visibility = "hidden";
 var warning = document.querySelector("p.caption1.caption2");
+var whowon = document.querySelector(".caption1");
 warning.style.visibility = "hidden";
+main1.style.visibility = "visible";
+main2.style.visibility = "hidden";
 iswarning = false;
 function checkinput() {
   var text = document.querySelector("input").value;
@@ -26,8 +33,44 @@ function checkinput() {
   } else if (text1 > 6 || text1 < 1) {
     warning.textContent = "❌ Dices-1:6.";
     warning.style.visibility = "visible";
+  } else {
+    startGame.style.visibility = "hidden";
+    createcontent.style.visibility = "visible";
+    warning.style.visibility = "hidden";
+
   }
 
 }
+function imgChange() {
+  main1.style.visibility = "hidden";
+  main2.style.visibility = "visible";
+  main2.style.display = "block";
+  main2.style.position = "relative";
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var num = document.querySelector(".players img");
+  var num1 = document.querySelectorAll(".players img")[1];
+
+  num.setAttribute("alt", randomNumber1.toString());
+  num.setAttribute("src", `./img/Dice-${randomNumber1}.png`);
+  num1.setAttribute("alt", randomNumber2.toString());
+  num1.setAttribute("src", `./img/Dice-${randomNumber2}.png`);
+  console.log(Number(num.getAttribute('alt')));
+  console.log(Number(num1.getAttribute('alt')));
+  if (Number(num.getAttribute("alt")) > Number(num1.getAttribute("alt"))) {
+    whowon.innerHTML = "Player 1 Won!";
+
+  } else if (Number(num.getAttribute("alt")) < Number(num1.getAttribute("alt"))) {
+    whowon.innerHTML = "Player 2 Won!";
+  } else if (Number(num.getAttribute("alt")) === Number(num1.getAttribute("alt"))) {
+    whowon.innerHTML = "It's A Draw!";
+  }
+
+
+}
+
+
+
 
 
